2755045a95cee157fad235d137900be0cd993853
Klecianny Melo <klecianny.melo@quorum.us>
2025-06-26 10:33:55 -0300

    :construction: example of code in the 66 page

--- chapters/4. Comments.py
+++ chapters/4. Comments.py
@@ -1,4 +1,4 @@
-# "O único comentário verdadeiramente bom é aquele em que você encontrou uma forma para não escreve-lo."
+# "O único comentário verdadeiramente bom é aquele em que você encontrou uma forma para não escreve-lo."- page 55
 
 # Add only even numbers to the result
 if number % 2 == 0:
@@ -10,3 +10,12 @@ def is_even(number):
 
 if is_even(number):
     result.append(number)
+
+
+# TODO
+
+# Page 66
+# "Troque a tentacao para criar ruidos pela determinacao para limpar seu codigo. Voce percebera que isso lhe tornara um programador melhor e mais feliz."
+
+# Old code my here
+# And the refactor here
