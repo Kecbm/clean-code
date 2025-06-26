1544bcd460faf671a819e0a4d276320a37e38b22
Klecianny Melo <klecianny.melo@quorum.us>
2025-06-26 17:30:04 -0300

    :construction: new phrase for chapter 5

--- chapters/5. Formatting.py
+++ chapters/5. Formatting.py
@@ -2,12 +2,7 @@
 
 # Page 78
 # "Cada linha em branco indica visualmente a separacao entre conceitos. [...] Retirar essas linhas em branco, gera um efeito. notavelmente obscuro na legibilidade do código."
-
-# Old code my here
-# And the refactor here
-
-# Page 85
-# "Qual deve ser o tamanho de uma linha? [...] Devemos nos esforcar para manter nossas linhas curtas. Ultrapassar 120 caracteres povavelmente é apenas falta de cuidado."
+# "Sem identacao, os programas seriam praticamente initeligiveis para humanos"
 
 # Old code my here
 # And the refactor here
