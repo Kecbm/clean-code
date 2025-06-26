2f0638fe0052989e97863137e0386b0439a2464d
Klecianny Melo <klecianny.melo@quorum.us>
2025-06-26 17:25:04 -0300

    :sparkles: final version of file 4

--- chapters/4. Comments.py
+++ chapters/4. Comments.py
@@ -1,5 +1,3 @@
-# "O único comentário verdadeiramente bom é aquele em que você encontrou uma forma para não escreve-lo."- page 55
-
 # Add only even numbers to the result
 if number % 2 == 0:
     result.append(number)
@@ -10,12 +8,3 @@ def is_even(number):
 
 if is_even(number):
     result.append(number)
-
-
-# TODO
-
-# Page 66
-# "Troque a tentacao para criar ruidos pela determinacao para limpar seu codigo. Voce percebera que isso lhe tornara um programador melhor e mais feliz."
-
-# Old code my here
-# And the refactor here
