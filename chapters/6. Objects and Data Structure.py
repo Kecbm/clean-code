f03f713fccf3894e4d315465913eb9ee27b4dbd7
Klecianny Melo <klecianny.melo@quorum.us>
2025-06-27 18:16:53 -0300

    :construction: objects and data structure chapter

---
+++ chapters/6. Objects and Data Structure.py
@@ -0,0 +1,5 @@
+# Page 95
+# "Os objetos usam abstracoes para esconder seus dados, e expoem as funcoes que operam em tais dados.
+# As estruturas de dados expoem seus dados e nao possuem funcoes significativas"
+
+# Code example in Python
