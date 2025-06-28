ff0412dd6edba17b8d15911e1b231b4ce60ab004
Klecianny Melo <klecianny.melo@quorum.us>
2025-06-28 15:47:14 -0300

    :hammer: new formate of code example in formattinh chapter

--- chapters/5. Formatting.py
+++ chapters/5. Formatting.py
@@ -1,24 +1,21 @@
-# TODO: Texto e imagem para postar no LinkedIn
-
 # Page 78
-# "Cada linha em branco indica visualmente a separacao entre conceitos. [...] Retirar essas linhas em branco, gera um efeito notavelmente obscuro na legibilidade do código."
+# "Cada linha em branco indica visualmente a separacao entre conceitos. [...]
+# Retirar essas linhas em branco, gera um efeito notavelmente obscuro na legibilidade do código."
 # "Sem identacao, os programas seriam praticamente initeligiveis para humanos"
 
-def calculate_discount(price, discount_percent):
-    discount_amount = price * (discount_percent / 100)
+def calculate_discount(price, percentage):
+    discount = price * (percentage / 100)
 
-    return discount_amount
-
-
-def calculate_tax(price, tax_percent):
-    tax_amount = price * (tax_percent / 100)
-
-    return tax_amount
+    return discount
 
 
-def calculate_final_price(original_price, discount_percent, tax_percent):
-    discount = calculate_discount(original_price, discount_percent)
-    price_after_discount = original_price - discount
-    final_price = price_after_discount + calculate_tax(price_after_discount, tax_percent)
-    
+def apply_discount(price, discount):
+    final_price = price - discount
+
     return final_price
+
+
+def calculate_savings(original_price, final_price):
+    savings = original_price - final_price
+
+    return savings
