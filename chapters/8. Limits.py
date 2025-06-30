ffde738eed2c1f9f61de346befa0bb8e981136ab
Klecianny Melo <klecianny.melo@quorum.us>
2025-06-30 20:30:17 -0300

    :construction: phrase about limits chapter

---
+++ chapters/8. Limits.py
@@ -0,0 +1,6 @@
+# Page 120
+# "É melhor depender de algo que voce controle do que pegar algo que acabe controlando voce"
+
+# EX.: Implementacao de uma api que torna fácil a mudaca de um gateway de pagamentos
+
+# Code example in Python
